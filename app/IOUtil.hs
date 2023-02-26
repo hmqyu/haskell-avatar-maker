808fa1c67dbfb953f5ee4195c3433f87f79c3666
sixverba <helenmqyu@gmail.com>
2023-02-26 15:38:45 -0800

    finished refactoring, removed avatar display gui for now. added short hairstyles

--- app/IOUtil.hs
+++ app/IOUtil.hs
@@ -1,30 +1,38 @@
-module IOUtil (loadAssets, formAssetPaths, outputPath) where
+module IOUtil (loadAssets, formAssetPaths, outputPath, colourTag, linesTag) where
 
 import Codec.Picture
-import AvatarMaker (avatarParts)
 
 imagesPath :: String
-imagesPath = "./images2/"
+imagesPath = "./avatar-parts/"
 
 outputPath :: String
 outputPath = "./output/"
 
-loadAssets :: String -> String -> String -> IO [Image PixelRGBA8]
-loadAssets currHairLength currHairTexture hasBangs = mapM loadImage (formAssetPaths currHairLength currHairTexture hasBangs)
+colourTag :: String
+colourTag = " colour.png"
 
-formAssetPaths :: String -> String -> String -> [String]
-formAssetPaths currHairLength currHairTexture hasBangs = 
-    foldr (\x y -> formAvatarPartPaths x ++ y) hairPaths avatarParts
-    where hairPaths = formHairPaths currHairLength currHairTexture hasBangs
+linesTag :: String
+linesTag = " lines.png"
 
-formAvatarPartPaths :: String -> [String]
-formAvatarPartPaths part = [imagesPath ++ part ++ " colour.png", imagesPath ++ part ++ " lines.png"]
+bodyParts :: [String]
+bodyParts = ["skin", "shirt", "eyes"]
 
-formHairPaths :: String -> String -> String -> [String]
-formHairPaths hairTexture hairLength hasBangs
-    | hasBangs == "yes" = hairPaths ++ formAvatarPartPaths "bangs"
-    | otherwise = hairPaths
-    where hairPaths = formAvatarPartPaths ("hair " ++ hairTexture ++ " " ++ hairLength)
+loadAssets :: String -> String -> String -> String -> IO [Image PixelRGBA8]
+loadAssets currHairTexture currHairLength hasBangs tag = mapM loadImage (formAssetPaths currHairTexture currHairLength hasBangs tag)
+
+formAssetPaths :: String -> String -> String -> String -> [String]
+formAssetPaths currHairLength currHairTexture hasBangs tag = 
+    let hairPaths = formHairPaths currHairLength currHairTexture hasBangs tag
+    in foldr (\x y -> formAvatarPartPaths x tag ++ y) hairPaths bodyParts
+
+formAvatarPartPaths :: String -> String -> [String]
+formAvatarPartPaths part tag = [imagesPath ++ part ++ tag]
+
+formHairPaths :: String -> String -> String -> String -> [String]
+formHairPaths hairTexture hairLength hasBangs tag =
+    let hairPaths = formAvatarPartPaths ("hair " ++ hairTexture ++ " " ++ hairLength) tag in
+    if hasBangs == "yes" then hairPaths ++ formAvatarPartPaths "bangs" tag
+    else hairPaths
 
 loadImage :: FilePath -> IO (Image PixelRGBA8)
 loadImage path = do
@@ -35,52 +43,3 @@ loadImage path = do
             let image :: Image PixelRGBA8
                 image = convertRGBA8 dynamicImage
             return image
-
--- formAssets :: String -> String -> String -> String -> String -> String -> String -> IO [Image PixelRGBA8]
--- formAssets hairColour hairLength hairTexture skinColour eyeColour shirtColour hasBangs = do
---     eyeImage <- mergeEye eyeColour
---     hairImage <- mergeHair hairColour hairLength hairTexture
---     baseImage <- mergeBase skinColour shirtColour
---     bangsImage <- if hasBangs == "yes" then mergeBangs hairColour else loadImage(formPath "blank")
---     return [baseImage, hairImage, bangsImage, eyeImage]
-
-
--- mergeBangs :: String -> IO(Image PixelRGBA8)
--- mergeBangs hairColour = do
---     lineImage <- loadImage (formPath "bangs lines")
---     colourImage <-  loadImage (formPath "bangs colour")
-    
---     let colourImageNew = dyeImage colourImage (colourToRGBA8 hairColour)
---     return (mergeImages lineImage colourImageNew)
-
-
--- mergeEye :: String -> IO(Image PixelRGBA8)
--- mergeEye eyeColour = do
---     lineImage <- loadImage (formPath "eyes lines")
---     colourImage <-  loadImage (formPath "eyes colour")
-    
---     let colourImageNew = dyeImage colourImage (colourToRGBA8 eyeColour)
---     return (mergeImages lineImage colourImageNew)
-
-
--- mergeBase :: String -> String -> IO(Image PixelRGBA8)
--- mergeBase skinColour shirtColour = do
---     baseLineImage <- loadImage (formPath "base lines")
---     baseColourImage <- loadImage (formPath "base colour")
---     shirtColourImage <- loadImage (formPath "shirt colour")
-
---     let 
---         baseColourImageNew = dyeImage baseColourImage (colourToRGBA8 skinColour)
---         shirtColourImageNew = dyeImage shirtColourImage (colourToRGBA8 shirtColour)
---     return (mergeImages baseLineImage (mergeImages baseColourImageNew shirtColourImageNew))
-
-
-
--- mergeHair :: String -> String -> String -> IO(Image PixelRGBA8)
--- mergeHair hairColour hairLength hairTexture = do
---     colourImage <- loadImage (formPath ("hair " ++ hairTexture ++ " " ++ hairLength ++ " colour"))
---     lineImage <- loadImage (formPath ("hair " ++ hairTexture ++ " " ++ hairLength ++ " lines"))
-
---     let colourImageNew = dyeImage colourImage (colourToRGBA8 hairColour)
---     return (mergeImages lineImage colourImageNew)
-    
--- src/AvatarMaker.hs
+++ src/AvatarMaker.hs
@@ -1,5 +1,5 @@
 module AvatarMaker
-    ( hairColours, hairLengths, hairTextures, skinColours, eyeColours, shirtColours, yesNo, createAvatar, colourToRGBA8, mergeImages, dyeImage, avatarParts ) where
+    ( AvatarPart(..), apOptions, createAvatar ) where
 
 ---------------
 --- IMPORTS ---
@@ -10,29 +10,17 @@ import Codec.Picture
 ---------------
 -- CONSTANTS --
 ---------------
-avatarParts :: [String]
-avatarParts = ["shirt", "skin", "eyes", "hair", "bangs"]
-
-hairColours :: [String]
-hairColours = ["black", "light brown", "dark brown", "blonde"]
-
-hairLengths :: [String]
-hairLengths = ["short","medium","long"] 
-
-hairTextures :: [String]
-hairTextures = ["straight","wavy","curly"] 
-
-skinColours :: [String]
-skinColours = ["light","tan","medium", "dark"] 
-
-eyeColours :: [String]
-eyeColours = ["blue","green","light brown", "dark brown"]
-
-shirtColours :: [String]
-shirtColours = ["blue","green","red", "black", "white"]
-
-yesNo :: [String]
-yesNo = ["yes","no"]
+-- avatar part constants --
+data AvatarPart = HairColour | HairLength | HairTexture | HasBangs | EyeColour | SkinColour | ShirtColour
+    deriving (Eq, Show)
+apOptions :: AvatarPart -> [String]
+apOptions HairColour = ["black", "light brown", "dark brown", "blonde"]
+apOptions HairLength = ["short","medium","long"]
+apOptions HairTexture = ["straight","wavy","curly"]
+apOptions HasBangs = ["yes","no"]
+apOptions EyeColour = ["blue","green","light brown", "dark brown"]
+apOptions SkinColour = ["light","tan","medium", "dark"]
+apOptions ShirtColour = ["blue","green","red","black", "white"]
 
 -- colour constants --
 colourToRGBA8 :: String -> PixelRGBA8
@@ -48,7 +36,7 @@ colourToRGBA8 "light"   = PixelRGBA8 181 140 104 255
 colourToRGBA8 "tan"     = PixelRGBA8 244 195 161 255
 colourToRGBA8 "medium"  = PixelRGBA8 204 154 128 255
 colourToRGBA8 "dark"    = PixelRGBA8 134 90 78 255
-colourToRGBA8 _    = PixelRGBA8 0 0 0 0
+colourToRGBA8 _ = PixelRGBA8 0 0 0 0
 
 
 ---------------
@@ -57,8 +45,11 @@ colourToRGBA8 _    = PixelRGBA8 0 0 0 0
 -- TODO:
 -- createAvatar 
 -- creates the avatar using the given components
-createAvatar :: [Image PixelRGBA8] -> [String] -> Image PixelRGBA8
-createAvatar parts colours = mergeAllImages (dyeAvatarParts parts (convertStringToPixelRGBA8 colours))
+createAvatar :: [Image PixelRGBA8] -> [Image PixelRGBA8] -> [String] -> Image PixelRGBA8
+createAvatar flats linearts colours = mergeAllImages (weaveLayers (dyeAvatarParts flats (convertStringToPixelRGBA8 colours)) linearts)
+
+weaveLayers :: [Image PixelRGBA8] -> [Image PixelRGBA8] -> [Image PixelRGBA8]
+weaveLayers flats linearts = foldr (\(flat, lineart) y -> [flat, lineart] ++ y) [] (zip flats linearts)
 
 mergeAllImages :: [Image PixelRGBA8] -> Image PixelRGBA8
 mergeAllImages (h:t) = foldl (\x y -> mergeImages y x) h t
@@ -97,8 +88,8 @@ dyeAvatarParts images colours = map dyeImage (zip images colours)
 
 -- dyes an image either using a defined colour constant
 dyeImage :: (Image PixelRGBA8, PixelRGBA8) -> Image PixelRGBA8
-dyeImage (img, PixelRGBA8 r1 g1 b1 _) =
-  pixelMap (\(PixelRGBA8 r2 g2 b2 a2) -> if a2 == 0 then PixelRGBA8 r2 g2 b2 a2 else PixelRGBA8 r1 g1 b1 a2) img
+dyeImage (img, PixelRGBA8 r1 g1 b1 a1) =
+  pixelMap (\(PixelRGBA8 r2 g2 b2 a2) -> if a2 == 0 then PixelRGBA8 r2 g2 b2 a2 else PixelRGBA8 r1 g1 b1 a1) img
 
 convertStringToPixelRGBA8 :: [String] -> [PixelRGBA8]
 convertStringToPixelRGBA8 colours = [colourToRGBA8 x | x <- colours]
