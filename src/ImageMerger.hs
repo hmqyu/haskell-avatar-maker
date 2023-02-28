e1c2a24771b9898cb9edf3de00dd0075bf2a2db5
sixverba <helenmqyu@gmail.com>
2023-02-27 22:26:52 -0800

    fixed up pixel colouring, colours, and README file

--- src/ImageMerger.hs
+++ src/ImageMerger.hs
@@ -1,3 +1,5 @@
+-- represents a set of functions used to merge multiple images together
+-- merging takes into account the transparency of the image(s)
 module ImageMerger ( mergeAllImages ) where
 
 ---------------
@@ -7,39 +9,53 @@ import Codec.Picture
 
 
 ---------------
+-- CONSTANTS --
+---------------
+-- represents the base image width and height
+imgWidth :: Int
+imgWidth = 896
+imgHeight :: Int
+imgHeight = 1046
+
+
+---------------
 -- FUNCTIONS --
 ---------------
 -- flattens all images into a single image from left to right
--- the first image in the array is the base
+-- assumes images in the list are all the same size
+-- the base image is an empty image
 mergeAllImages :: [Image PixelRGBA8] -> Image PixelRGBA8
--- base case impossible to reach in Main.hs, but added regardless; generates a 1x1 empty image
-mergeAllImages [] = generateImage (\x y -> PixelRGBA8 (fromIntegral x) (fromIntegral y) 0 0) 1 1
-mergeAllImages (h:t) = foldl (\x y -> mergeImages y x) h t
+mergeAllImages = mergeImageWithList base where
+    mergeImageWithList img [] = img
+    mergeImageWithList img (h:t) = mergeImageWithList (mergeImages h img) t
+    base = generateImage (\x y -> PixelRGBA8 (fromIntegral x) (fromIntegral y) 0 0) imgWidth imgHeight
 
 -- merges two PNG images together into one PNG image
 -- the first image passed will be merged on top of the second image passed
 mergeImages :: Image PixelRGBA8 -> Image PixelRGBA8 -> Image PixelRGBA8
-mergeImages topImage bottomImage = generateImage replacePixel width height
+mergeImages topImage bottomImage = generateImage mergePixel width height
   where
-    replacePixel x y 
+    mergePixel x y 
         | pixelOpacity (pixelAt topImage x y) == 0 = pixelAt bottomImage x y
-        | pixelOpacity (pixelAt topImage x y) < 255 = mixPixel (pixelAt topImage x y) (pixelAt bottomImage x y)
-        | otherwise = pixelAt topImage x y
-    width = imageWidth bottomImage
-    height = imageHeight bottomImage
+        | (pixelOpacity (pixelAt bottomImage x y) == 0) || (pixelOpacity (pixelAt topImage x y) == 255) = pixelAt topImage x y
+        | otherwise = findPixelRGBA8 (pixelAt topImage x y) (pixelAt bottomImage x y)
+    width = imageWidth topImage
+    height = imageHeight topImage
 
 -- deals with pixels that have transparency (alpha < 1) 
 -- determines what colour a pixel should be if a pixel with transparency is layered on top
 -- the first pixel passed is layered on top of the second pixel passed
-mixPixel :: PixelRGBA8 -> PixelRGBA8 -> PixelRGBA8
-mixPixel (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) =
+findPixelRGBA8 :: PixelRGBA8 -> PixelRGBA8 -> PixelRGBA8
+findPixelRGBA8 (PixelRGBA8 r1 g1 b1 a1) (PixelRGBA8 r2 g2 b2 a2) =
     if a2 == 0 then PixelRGBA8 r1 g1 b1 a1
     else PixelRGBA8
-        (mixPixelValue r1 r2 a1)
-        (mixPixelValue g1 g2 a1)
-        (mixPixelValue b1 b2 a1)
-        (mixPixelValue a1 a2 a1)
+        (findColourChannelValue r1 r2 a1)
+        (findColourChannelValue g1 g2 a1)
+        (findColourChannelValue b1 b2 a1)
+        255 
+-- set alpha = 255 because it's impossible for mixPixel to be called on an a2 < 255 in our program
+-- a2 must be 255 here, or a combination of a1 and a2 results in 255
 
 -- determines the colour value if a colour with transparency is layered on top
-mixPixelValue :: (Integral a) => a -> a -> a -> a
-mixPixelValue c1 c2 alpha = round ((fromIntegral alpha / 255.0) * fromIntegral c1 + (fromIntegral (255 - alpha) / 255.0) * fromIntegral c2)
+findColourChannelValue :: (Integral a) => a -> a -> a -> a
+findColourChannelValue ccv1 ccv2 alpha = round ((fromIntegral alpha / 255.0) * fromIntegral ccv1 + (fromIntegral (255 - alpha) / 255.0) * fromIntegral ccv2)
